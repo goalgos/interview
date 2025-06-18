9ba39f45b108c3c80c7194aa40d3642a1902d605
Илья <ostapich77@gmail.com>
2025-06-19 01:04:19 +0300

    Hey^-^

--- leetcode/merge/main.go
+++ leetcode/merge/main.go
@@ -1,1 +1,32 @@
-package merge
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func merge(intervals [][]int) [][]int {
+	if len(intervals) == 0 {
+		return intervals
+	}
+	sort.Slice(intervals, func(i, j int) bool {
+		return intervals[i][0] < intervals[j][0]
+	})
+
+	result := [][]int{intervals[0]}
+	for _, cur := range intervals[1:] {
+		previous := result[len(result)-1]
+		if cur[0] < previous[1] {
+			previous[1] = max(previous[1], cur[1])
+		} else {
+			result = append(result, cur)
+		}
+	}
+
+	return result
+}
+
+func main() {
+	intervals := [][]int{{1, 3}, {2, 6}, {8, 10}, {15, 18}}
+	fmt.Println(merge(intervals))
+}
--- leetcode/my_atoi/main.go
+++ leetcode/my_atoi/main.go
@@ -1,1 +1,43 @@
-package my_atoi
+package main
+
+import "fmt"
+
+func myAtoi(s string) int {
+	maxInt := 2147483647
+	minInt := -2147483648
+	runes := []rune(s)
+	n := len(runes)
+	i := 0
+	for i < n && runes[i] == ' ' {
+		i++
+	}
+
+	sign := 1
+	if i < n {
+		if i < n && runes[i] == '-' {
+			sign = -1
+			i++
+		} else if runes[i] == '+' {
+			i++
+		}
+	}
+
+	result := 0
+	for i < n && runes[i] >= '0' && runes[i] <= '9' {
+		digit := int(runes[i] - '0')
+		if sign == 1 && (result > (maxInt-digit)/10) {
+			return maxInt
+		}
+		if sign == -1 && (result > (-minInt-digit)/10) {
+			return minInt
+		}
+		result = result*10 + digit
+		i++
+	}
+
+	return sign * result
+}
+
+func main() {
+	fmt.Println(myAtoi("   -042"))
+}
